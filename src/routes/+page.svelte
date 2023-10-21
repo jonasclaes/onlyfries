b8c4982e643746478bd7f0d0d99d2f3f0ed34073
Jonas Claes <jonas@jonasclaes.be>
2023-10-21 13:06:37 +0000

    add temporary notice of construction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to OnlyFries!</h1>
+<sub>The place to be for everything fries related.</sub>
+<h2>Construction</h2>
+<p>
+    This website is currently under construction. Please come back later.
+</p>
+<footer>
+    <h2>Footer</h2>
+    <p>
+        This website is an initiative by <a href="https://jonasclaes.be?ref=onlyfri.es" target="_blank">jonasclaes.be</a>.
+    </p>
+</footer>
