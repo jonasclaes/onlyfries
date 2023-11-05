a1545a6e017196039e03e70bcbfcd8280264223c
Jonas Claes <jonas@jonasclaes.be>
2023-11-05 11:20:06 +0000

    add TailwindCSS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
-<h1>Welcome to OnlyFries!</h1>
+<h1 class="text-3xl">Welcome to OnlyFries!</h1>
 <sub>The place to be for everything fries related.</sub>
-<h2>Construction</h2>
-<p>
-    This website is currently under construction. Please come back later.
-</p>
+<h2 class="text-2xl">Construction</h2>
+<p>This website is currently under construction. Please come back later.</p>
 <footer>
-    <h2>Footer</h2>
-    <p>
-        This website is an initiative by <a href="https://jonasclaes.be?ref=onlyfri.es" target="_blank">jonasclaes.be</a>.
-    </p>
+	<h2 class="text-2xl">Footer</h2>
+	<p>
+		This website is an initiative by <a href="https://jonasclaes.be?ref=onlyfri.es" target="_blank"
+			>jonasclaes.be</a
+		>.
+	</p>
 </footer>
